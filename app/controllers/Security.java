732e467b000393de4ce805ad167053e86a16d2d3
Vlastimil Dolejs <vlasta.dolejs@gmail.com>
2014-06-14 17:32:33 +0200

    default admin password for development

--- app/controllers/Security.java
+++ app/controllers/Security.java
@@ -1,24 +1,48 @@
 /**
- * 
+ *
  */
 package controllers;
+
+import org.apache.log4j.Logger;
+
+import play.Play;
 
 /**
  * @author Vlastimil Dolejs (vlasta.dolejs@gmail.com)
  *
  */
 public class Security extends controllers.Secure.Security {
-	
+
+	private static final Logger log = play.Logger.log4j;
+
 	static boolean authenticate(String username, String password) {
-		
-		String adminLogin = System.getProperty("kontaktynaurady.admin.username");
-		String adminPassword = System.getProperty("kontaktynaurady.admin.password");
-		
+
+		String adminLogin = Play.configuration.getProperty("kontaktynaurady.admin.username");
+		String adminPassword = Play.configuration.getProperty("kontaktynaurady.admin.password");
+
+		if (adminLogin == null) {
+			adminLogin = System.getProperty("kontaktynaurady.admin.username");
+		}
+
+		if (adminPassword == null) {
+			adminPassword = System.getProperty("kontaktynaurady.admin.password");
+		}
+
+		if (adminLogin == null) {
+			log.warn("System property 'kontaktynaurady.admin.username' is not set");
+			return false;
+		}
+
+		if (adminPassword == null) {
+			log.warn("System property 'kontaktynaurady.admin.password' is not set");
+			return false;
+		}
+
 		if (adminLogin.equals(username) && adminPassword.equals(password)) {
 			return true;
 		}
-		
+
 		return false;
     }
-	
+
 }
