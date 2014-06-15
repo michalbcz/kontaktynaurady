1845bfa3e7407b4edd2824f58ee6f38650832da3
Michal Bernhard <michal@bernhard.cz>
2014-06-15 17:18:14 +0200

    zlepsen example - example aplikace zobrazuje info u markeru

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -13,12 +13,21 @@ $(function() {
 
 	function displayOrganizations(data) {
 		$.each(data, function(index, value) {
-			
+
+            var infowindow = new google.maps.InfoWindow();
+
 			var marker = new google.maps.Marker({
 			    position: new google.maps.LatLng(value.latitude, value.longitude),
 			    map: map,
 			    title: value.name
 			});
+
+            google.maps.event.addListener(marker, 'click', function() {
+                infowindow.setContent(
+                                value.name + "<br/>" + value.addressStreet + "<br/>" + value.addressZipCode + " " + value.addressCity + "<br/>" +
+                                "<a href=\"" + value.www + "\">" + value.www + "</a>"  + " / " + "<a href=\"mailto:" + value.email + "\">" + value.email + "</a>");
+                infowindow.open(map, this);
+            });
 			
 		});
 	}
