9f8dc5337d8fb095bc19a9e28ccaccd38cfe22bd
Michal Bernhard <michal@bernhard.cz>
2014-06-15 10:18:34 +0200

    JSONP support didn't work

--- app/controllers/OrganizationsRest.java
+++ app/controllers/OrganizationsRest.java
@@ -7,14 +7,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import com.google.common.base.Joiner;
 import com.google.gson.Gson;
 import models.Organization;
 
 import org.apache.commons.lang.StringUtils;
 
+import play.db.jpa.JPA;
 import play.mvc.Controller;
 
 import com.google.common.collect.Lists;
+
+import javax.persistence.Query;
 
 /**
  * @author Vlastimil Dolejs (vlasta.dolejs@gmail.com)
@@ -34,10 +38,17 @@ public class OrganizationsRest extends Controller {
 		List<String> queryParams = Lists.newArrayList();
 
 		for (Entry<String, String> entry : parameters.entrySet()) {
+
 			String fieldName = entry.getKey();
 			fieldName = StringUtils.lowerCase(fieldName);
 			fieldName = StringUtils.capitalize(fieldName);
-			
+
+            if (StringUtils.containsIgnoreCase(fieldName, "radius")
+                    || StringUtils.containsIgnoreCase(fieldName, "callback")) {
+                // skip as it's not Organization's field name
+                continue;
+            }
+
 			String value = entry.getValue();
 			value = value.replace("*", "%");
 			
@@ -58,10 +69,36 @@ public class OrganizationsRest extends Controller {
 			organizations = Organization.find(queryFindBy, queryParams.toArray()).fetch();
 		}
 
-        if (queryParams.contains("callback")) {
+        if (parameters.containsKey("radius")) {
+
+            Float radiusAroundLatitude = Float.valueOf(parameters.get("radiusAroundLatitude"));
+            Float radiusAroundLongitude = Float.valueOf(parameters.get("radiusAroundLongitude"));
+            Long radius = Long.valueOf(parameters.get("radius"));
+
+            List<Long> organizationsIds = Lists.newArrayList();
+            for (Organization organization : organizations) {
+               organizationsIds.add(organization.getId());
+            }
+
+            Query query = JPA.em().createNativeQuery(
+                    "SELECT * FROM organization " +
+                            "WHERE ST_Intersects(" +
+                            "   ST_GeographyFromText('SRID=4326;POINT(' || latitude || ' ' || longitude || ')')," +
+                            "   ST_Buffer(ST_GeographyFromText('SRID=4326;POINT(" + radiusAroundLatitude + " " + radiusAroundLongitude + ")'), " +  radius + ")) AND id IN (" +
+                            Joiner.on(",").join(organizationsIds) +
+                            ");", Organization.class);
+
+            List<Organization> radiusFilteredOrganization = query.getResultList();
+
+            organizations = radiusFilteredOrganization;
+
+        }
+
+
+        if (parameters.containsKey("callback")) {
             Gson gson = new Gson();
             String out = gson.toJson(organizations);
-            renderText(request.params.get("callback") + "(" + out + ")");
+            renderText(parameters.get("callback") + "(" + out + ")");
         } else {
             renderJSON(organizations);
         }
