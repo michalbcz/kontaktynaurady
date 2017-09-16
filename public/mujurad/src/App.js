f409bcc094e3abe1b4375e0dba94fbc1f9be3eed
michalbcz <michal@bernhard.cz>
2017-09-16 02:39:41 +0200

    add mujurad initial commit

---
+++ public/mujurad/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+   constructor(props) {
+    super(props);
+    this.state = {name: props.name};
+  }
+
+  componentDidMount() {
+    this.setState({ name: 'Michal'})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React, { this.state.name }</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <input type="text" />
+      </div>
+    );
+  }
+}
+
+export default App;
